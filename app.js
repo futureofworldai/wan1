b1df22012a0347523c9642f3348e4ea05ffe6a31
student <dhirajchavan355@gmail.com>
2024-01-13 14:32:24 +0530

    add new function

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const wrapAsync=require('./utils/wrapAsync.js');
 const ExpressError=require('./utils/ExpressError.js');
 const {listingSchema,reviewSchema}=require('./schema.js');
 const Review=require('./models/review.js');
+const { wrap } = require('module');
 main()
   .then(()=>{
     console.log('Connected to DB')
@@ -106,6 +107,12 @@ app.put("/listings/:id",validateListing, wrapAsync(async (req, res) => {
    res.redirect(`/listings/${listing._id}`);
   }));
 
+  app.delete("/listings/:id/reviews/:reviewId",wrapAsync(async(req,res)=>{
+    let {id,reviewId}=req.params;
+    await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/listings/${id}`);
+  }))
 
 // app.get("/testlisting",async (req,res)=>{
 
--- models/listing.js
+++ models/listing.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
+const Review=require('./review.js');
 const listingSchamma=new Schema({
     title:{
         type:String,
@@ -32,6 +33,12 @@ const listingSchamma=new Schema({
     }]
 
 });
+listingSchamma.post('findOneAndDelete',async(listing)=>{
+if(listing){
+    await Review.deleteMany({_id:{$in:listing.reviews}});
+}
+})
+
 
 const Listing=mongoose.model('Listing',listingSchamma);
 
